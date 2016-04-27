469796adc7a1b356c1b0cc614a7213eccbb3367a
Nicolas Cavigneaux <nico@bounga.org>
2016-04-27 18:54:23 +0200

    Add 'es6' extension to js2-mode

--- modules/settings-js.el
+++ modules/settings-js.el
@@ -10,6 +10,7 @@
 (require 'json-mode)
 
 (add-to-list 'auto-mode-alist '("\\.js\\'"    . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.es6\\'"    . js2-mode))
 
 (setq-default js2-basic-offset 2)
 
