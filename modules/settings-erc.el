85b12affd3740f9c8c66abff03086751fca24c56
Nicolas Cavigneaux <nico@bounga.org>
2016-08-23 15:10:58 +0200

    Update ERC config to auto-identify on connect

--- modules/settings-erc.el
+++ modules/settings-erc.el
@@ -6,20 +6,31 @@
 
 ;;; Code:
 
-(defvar erc-hide-list '("JOIN" "PART" "QUIT"))
-(defvar erc-kill-buffer-on-part t)
-(defvar erc-kill-queries-on-quit t)
-(defvar erc-kill-server-buffer-on-quit t)
-(defvar erc-server-coding-system '(utf-8 . utf-8))
-(defvar erc-modules
-   (quote
-    (autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands notifications readonly ring services smiley stamp track)))
+(require 'erc)
+(require 'erc-services)
 
-(defvar erc-nick "Bounga")
-(defvar erc-server "irc.freenode.net")
-(defvar erc-autojoin-channels-alist '(("freenode.net" "#ruby" "#emacs" "#emacsfr")))
+(load "~/.emacs.d/personal/ercpass.el.gpg")
+
+(setq erc-prompt-for-password nil
+      erc-prompt-for-nickserv-password nil
+      erc-hide-list '("JOIN" "PART" "QUIT")
+      erc-kill-buffer-on-part t
+      erc-kill-queries-on-quit t
+      erc-kill-server-buffer-on-quit t
+      erc-server-coding-system '(utf-8 . utf-8)
+      erc-modules
+      (quote
+       (autojoin button completion fill irccontrols match netsplit networks noncommands notifications readonly ring services smiley stamp track))
+      erc-nick "Bounga"
+      erc-user-full-name "Nicolas Cavigneaux"
+      erc-server "irc.freenode.net"
+      erc-autojoin-channels-alist '(("freenode.net" "#ruby" "#elixir-lang" "#emacs" "#emacsfr")))
 
 (erc-truncate-mode +1)
+(erc-services-mode 1)
+
+(setq erc-nickserv-passwords
+      `((freenode     (("nick-one" . ,freenode-pass)))))
 
 (provide 'settings-erc)
 ;;; settings-erc.el ends here
