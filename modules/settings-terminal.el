8b4100a1e26d6332055c0822561d78b5ec3a12f2
Nicolas Cavigneaux <nico@bounga.org>
2016-09-18 11:18:05 +0200

    Add settings for terminal usage inside emacs

---
+++ modules/settings-terminal.el
@@ -0,0 +1,26 @@
+;;; settings-terminal --- Module for terminal settings
+
+;;; Commentary:
+
+;; Terminal related configuration.
+
+;;; Code:
+
+(require 'multi-term)
+(setq multi-term-program "/usr/local/bin/zsh")
+
+;; Disable Yasnippet completion in terminal, use native one
+(add-hook 'term-mode-hook (lambda()
+                            (yas-minor-mode -1)))
+
+;; Ensure utf-8 is used to be able to display special characters
+(add-hook 'term-exec-hook
+          (function
+           (lambda ()
+             (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))))
+
+;; Bind C-c t shortcut to dedicated multi-term
+(global-set-key (kbd "C-c t") 'multi-term-dedicated-toggle)
+
+(provide 'settings-terminal)
+;;; settings-terminal ends here
