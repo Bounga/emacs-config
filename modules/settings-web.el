7ed60edf34bd91bee53618093eebab870f974181
Nicolas Cavigneaux <nico@bounga.org>
2016-10-03 18:25:37 +0200

    Use smartparens mode for web-mode

--- modules/settings-web.el
+++ modules/settings-web.el
@@ -9,11 +9,23 @@
 (use-package web-mode
   :ensure t
   :init
-  (add-hook 'web-mode-hook (lambda () (smartparens-mode -1)))
-
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.eex\\'" . web-mode)))
+  (add-to-list 'auto-mode-alist '("\\.eex\\'" . web-mode))
+
+  :config
+  ;; make web-mode play nice with smartparens
+  (setq web-mode-enable-auto-pairing nil)
+  (sp-with-modes '(web-mode)
+    (sp-local-pair "%" "%"
+                   :unless '(sp-in-string-p)
+                   :post-handlers '(((lambda (&rest _ignored)
+                                       (just-one-space)
+                                       (save-excursion (insert " ")))
+                                     "SPC" "=" "#")))
+    (sp-local-tag "%" "<% "  " %>")
+    (sp-local-tag "=" "<%= " " %>")
+    (sp-local-tag "#" "<%# " " %>")))
 
 (provide 'settings-web)
 ;;; settings-web ends here
