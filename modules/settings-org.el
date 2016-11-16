6048aedc57f0dfdf8e643c06fd937b6d142d9bd4
Nicolas Cavigneaux <nico@bounga.org>
2016-11-16 13:07:28 +0100

    Add config for capture and refilling

--- modules/settings-org.el
+++ modules/settings-org.el
@@ -22,6 +22,21 @@
         org-default-notes-file (concat org-directory "/notes.org")
         org-src-fontify-natively t)
 
+  ;; Capture templates
+  (defvar org-capture-templates)
+  (setq org-capture-templates
+      (quote (("t" "Task" entry (file+headline (concat org-directory "/notes.org") "Tasks")
+               "* TODO %?\n%u\n%a\n")
+              ("w" "Work" entry (file+headline (concat org-directory "/work.org") "Tasks")
+               "* TODO %?\n%u\n%a\n")
+              ("h" "Home" entry (file+headline (concat org-directory "/home.org") "Tasks")
+               "* TODO %?\n%u\n%a\n"))))
+
+  ;; targets for refiling
+  (setq org-refile-targets (quote (
+                                   (nil :maxlevel . 2)
+                                   (org-agenda-files :maxlevel . 2))))
+
   (add-hook 'org-mode-hook 'turn-on-auto-fill)
   (add-hook 'org-mode-hook 'flyspell-mode)
   :config
