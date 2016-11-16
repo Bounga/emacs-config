0083c00717942ee7ca0a0872bbc51ec6e00fbd91
Nicolas Cavigneaux <nico@bounga.org>
2016-11-16 11:03:24 +0100

    Add missing calendar related settings file

---
+++ modules/settings-calendar.el
@@ -0,0 +1,28 @@
+;;; settings-calendar.el --- Module for calendar setup
+
+;;; Commentary:
+
+;; This module setups calendar to fits my preferences
+
+;;; Code:
+
+(defun calendar-show-week (arg)
+  "Display week number in calendar-mode if ARG is t."
+  (interactive "P")
+  (copy-face font-lock-constant-face 'calendar-iso-week-face)
+  (set-face-attribute
+   'calendar-iso-week-face nil :height 0.7)
+  (setq calendar-intermonth-text
+        (and arg
+             '(propertize
+               (format
+                "%2d"
+                (car (calendar-iso-from-absolute
+                      (calendar-absolute-from-gregorian
+                       (list month day year)))))
+               'font-lock-face 'calendar-iso-week-face))))
+
+(calendar-show-week t)
+
+(provide 'settings-calendar)
+;;; settings-calendar.el ends here
