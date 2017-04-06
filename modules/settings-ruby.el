00acb5a31562f3e87828468658c0bc95eec3ea42
Nicolas Cavigneaux <nico@bounga.org>
2017-04-06 22:25:45 +0200

    Back to original ruby-mode and use rbenv only for ruby files

--- modules/settings-ruby.el
+++ modules/settings-ruby.el
@@ -9,45 +9,31 @@
 (use-package rbenv
   :ensure t
   :config
-  (global-rbenv-mode))
+  (add-hook 'ruby-mode-hook 'rbenv-use-corresponding))
 
-(use-package enh-ruby-mode
+(use-package ruby-mode
   :ensure t
   :init
-  (add-to-list 'auto-mode-alist '("\\.rb\\'" . enh-ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.rake\\'" . enh-ruby-mode))
-  (add-to-list 'auto-mode-alist '("Rakefile\\'" . enh-ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.gemspec\\'" . enh-ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.ru\\'" . enh-ruby-mode))
-  (add-to-list 'auto-mode-alist '("Gemfile\\'" . enh-ruby-mode))
-  (add-to-list 'auto-mode-alist '("Guardfile\\'" . enh-ruby-mode))
-  (add-to-list 'auto-mode-alist '("Capfile\\'" . enh-ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.cap\\'" . enh-ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.thor\\'" . enh-ruby-mode))
-  (add-to-list 'auto-mode-alist '("Thorfile\\'" . enh-ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.jbuilder\\'" . enh-ruby-mode))
-
-  (add-hook 'enh-ruby-mode-hook 'subword-mode)
+  (add-hook 'ruby-mode-hook 'subword-mode)
 
   :config
-  (setq enh-ruby-deep-indent-paren nil
-        enh-ruby-add-encoding-comment-on-save nil))
+  (setq ruby-deep-indent-paren nil))
 
 (use-package ruby-tools
   :ensure t
   :init
-  (add-hook 'enh-ruby-mode-hook 'ruby-tools-mode))
+  (add-hook 'ruby-mode-hook 'ruby-tools-mode))
 
 (use-package inf-ruby
   :ensure t
   :init
-  (add-hook 'enh-ruby-mode-hook 'inf-ruby-minor-mode))
+  (add-hook 'ruby-mode-hook 'inf-ruby-minor-mode))
 
 (use-package robe
   :ensure t
   :init
-  (add-hook 'enh-ruby-mode-hook 'robe-mode)
-  (add-hook 'enh-ruby-mode-hook 'eldoc-mode)
+  (add-hook 'ruby-mode-hook 'robe-mode)
+  (add-hook 'ruby-mode-hook 'eldoc-mode)
   :config
   (eval-after-load 'company
     '(push 'company-robe company-backends)))
@@ -55,7 +41,7 @@
 (use-package yard-mode
   :ensure t
   :init
-  (add-hook 'enh-ruby-mode-hook 'yard-mode))
+  (add-hook 'ruby-mode-hook 'yard-mode))
 
 (use-package align
   :ensure t
@@ -65,35 +51,32 @@
                '(ruby-comma-delimiter
                  (regexp . ",\\(\\s-*\\)[^# \t\n]")
                  (repeat . t)
-                 (modes  . '(enh-ruby-mode))))
+                 (modes  . '(ruby-mode))))
 
   (add-to-list 'align-rules-list
                '(ruby-hash-literal
                  (regexp . "\\(\\s-*\\)=>\\s-*[^# \t\n]")
                  (group 2 3)
                  (repeat . t)
-                 (modes  . '(enh-ruby-mode))))
+                 (modes  . '(ruby-mode))))
 
   (add-to-list 'align-rules-list
                '(ruby-hash-literal2
                  (regexp . "[a-z0-9]:\\(\\s-*\\)[^# \t\n]")
                  (repeat . t)
-                 (modes  . '(enh-ruby-mode))))
+                 (modes  . '(ruby-mode))))
 
   (add-to-list 'align-rules-list
                '(ruby-assignment-literal
                  (regexp . "\\(\\s-*\\)=\\s-*[^# \t\n]")
                  (repeat . t)
-                 (modes  . '(enh-ruby-mode))))
+                 (modes  . '(ruby-mode))))
 
   (add-to-list 'align-rules-list
                '(ruby-xmpfilter-mark
                  (regexp . "\\(\\s-*\\)# => [^#\t\n]")
                  (repeat . nil)
-                 (modes  . '(enh-ruby-mode)))))
-
-(use-package slim-mode
-  :ensure t)
+                 (modes  . '(ruby-mode)))))
 
 (provide 'settings-ruby)
 ;;; settings-ruby.el ends here
