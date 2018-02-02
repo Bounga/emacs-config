3d39d5ba1c46409b2374ac950f4093c417ef6c47
Nicolas Cavigneaux <nico@bounga.org>
2018-02-02 18:55:02 +0100

    Add more default for better work related tasks handling

--- modules/settings-org.el
+++ modules/settings-org.el
@@ -27,7 +27,16 @@
         org-default-notes-file (concat org-directory "/notes.org")
         org-src-fontify-natively t
         org-confirm-babel-evaluate nil
-        org-clock-idle-time 10)
+        org-clock-idle-time 10
+        org-global-properties '(("Effort_ALL". "0 0:10 0:20 0:30 1:00 2:00 3:00 4:00 6:00 8:00"))
+        org-columns-default-format '"%38ITEM(Details) %TAGS(Context) %7TODO(To Do) %5Effort(Time){:} %6CLOCKSUM(Clock)")
+
+  (add-to-list 'org-agenda-custom-commands
+               '("w" "Work agenda and TODOs"
+                 ((agenda)
+                  (tags-todo "CATEGORY=\"work\"")
+                  )
+                 ((org-agenda-category-filter-preset '("+work")))))
 
   ;; Capture templates
   (defvar org-capture-templates)
@@ -41,9 +50,9 @@
               ("h" "Home" entry (file+headline (lambda () (concat org-directory "/home.org")) "Tasks")
                "* TODO %?\n%u")
               ("l" "Link" entry (file+headline (lambda () (concat org-directory "/links.org")) "Links")
-               "* TODO %^L %^g \n%U" :prepend t)
+               "* TODO %^L %^g \n%u" :prepend t)
               ("j" "Journal" entry (file+olp+datetree (lambda () (concat org-directory "/journal.org")))
-               "* %?\nEntered on %U\n  %i\n  %a" :clock-in t :clock-resume t :clock-keep t)
+               "* %?\nEntered on %U\n  %i" :clock-in t :clock-resume t :clock-keep t)
               ("a" "Appointment" entry (file (lambda () (concat org-directory "/gcal.org")))
                "* %?\n\n%^T\n\n:PROPERTIES:\n\n:END:\n\n"))))
 
