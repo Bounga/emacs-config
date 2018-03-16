b6e470b117467004a6b625381d98d2610a41872f
Nicolas Cavigneaux <nico@bounga.org>
2018-03-16 22:45:26 +0100

    Much faster and natural company completion

--- modules/settings-editing.el
+++ modules/settings-editing.el
@@ -29,10 +29,16 @@
 (use-package company
   :diminish company-mode
   :config
-  (setq company-idle-delay 0.5
+  (setq company-idle-delay 0
         company-tooltip-limit 10
-        company-minimum-prefix-length 2)
+        company-minimum-prefix-length 3)
   (global-company-mode t))
+
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
 
 (use-package company-dabbrev
   :config
