84075acc1a0eec1d50b737879b52a507c169f0b4
Nicolas Cavigneaux <nico@bounga.org>
2019-07-10 15:29:27 +0200

    Use Newsticker rather than Gnus to read news feeds

--- gnus.el
+++ gnus.el
@@ -99,22 +99,5 @@
 (mailcap-add-mailcap-entry "application" "*" '((viewer . "open %s") (type . "application/*")))
 (mailcap-add-mailcap-entry "image" "*" '((viewer . "open %s") (type . "image/*")))
 
-;; Convert Atom feeds to RSS feeds readable by Gnus
-(require 'mm-url)
-(defadvice mm-url-insert (after DE-convert-atom-to-rss () )
-  "Convert atom to RSS by calling xsltproc."
-  (when (re-search-forward "xmlns=\"http://www.w3.org/.*/Atom\""
-               nil t)
-    (message "Converting Atom to RSS... ")
-    (goto-char (point-min))
-    (call-process-region (point-min) (point-max)
-             "xsltproc"
-             t t nil
-             (expand-file-name "~/.emacs.d/atom2rss.xsl") "-")
-    (goto-char (point-min))
-    (message "Converting Atom to RSS... done")))
-
-(ad-activate 'mm-url-insert)
-
 (provide 'gnus)
 ;;; gnus.el ends here
